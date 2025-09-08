1f63103c6e1003e61099d713cebec8c77c72b28c
Tan-b29 <b2029amdrtan@pshs.edu.ph>
2025-09-08 15:17:35 +0800

    practice notes

---
+++ heh2.js
@@ -0,0 +1,49 @@
+//repetitive
+console.log (1)
+console.log (2)
+console.log (3)
+
+//   start      condition update
+for (let i = 2; i<=20; i=i+2 ) {
+    console.log(i)
+}
+
+//second type of for loop
+fav_fruits = ["mango", "banana", "apple"];
+fav_fruits.push("strawberry")
+var popped = fav_fruits.pop()
+
+console.log(popped)
+
+// for loop
+for(let i=0; i<fav_fruits.length; i++){
+    console.log(fav.fruits[i])
+}
+
+//python type shift
+for (fruit of fav_fruits) {
+    console.log(fruit)
+}
+
+//favorite ni sir roy
+fav_fruits.forEach(print_fruit)
+
+function print_fruit(fruit){
+    console.log(fruit)
+}
+
+//while loops
+i = 1;
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+//do while loops
+i=1;
+do {
+     console.log (i);
+     i++
+} while (i<=3);
+
+//second type for loop
